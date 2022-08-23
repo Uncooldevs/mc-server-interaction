601275e3b7167363f13ad1fd3871ca9e5f832e10
Dummerle <44114474+Dummerle@users.noreply.github.com>
2022-08-23 22:25:28 +0200

    Add some necessary functions to property_handler.py

--- mc_server_interaction/property_handler.py
+++ mc_server_interaction/property_handler.py
@@ -1,9 +1,13 @@
+from typing import Optional
+
+
 class ServerProperties:
     __data: dict
 
     def __init__(self, file_name: str):
+        self.file_name = file_name
         self.__data = {}
-        with open(file_name, "r") as f:
+        with open(file_name, "r", encoding="utf-8") as f:
             for i in f:
                 if i.startswith("#"):
                     continue
@@ -21,8 +25,24 @@ class ServerProperties:
 
                 self.__data[key] = value
 
-    def get(self, key, fallback):
+    def set(self, key, value):
+        self.__data[key] = value
+
+    def get(self, key, fallback=None):
         return self.__data.get(key, fallback)
 
     def __str__(self):
         return str(self.__data)
+
+    def save(self, override_filename: Optional[str] = None):
+        with open(override_filename or self.file_name, "w", encoding="utf-8") as f:
+            for key, raw_value in self.__data.items():
+                if type(raw_value) == bool:
+                    value = str(raw_value).lower()
+                elif raw_value is None:
+                    value = ""
+                else:
+                    value = str(raw_value)
+                f.write(f"{key}={value}\n")
+
+
