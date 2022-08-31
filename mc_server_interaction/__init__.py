85679fda40462620165a5d652ae3e2134a178b03
M0rica <afer2003.mueller@gmail.com>
2022-08-31 19:31:19 +0200

    Make app singleton

--- mc_server_interaction/__init__.py
+++ mc_server_interaction/__init__.py
@@ -2,5 +2,8 @@
 __version__ = "0.1.0"
 __app_name__ = "mc-server-interaction"
 
+from mc_server_interaction.utils.singleton import SingleInstance
+singleton = SingleInstance()
+
 import mc_server_interaction.log as log
 log.setup_logging()
